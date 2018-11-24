5ce3cecd35b8a6124550ba398b7150eff3645669
rajat agarwal <rajat@rajats-MacBook-Air.local>
2018-11-24 15:48:02 -0800

    clear

--- inf551/inf551/static/main.js
+++ inf551/inf551/static/main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	$('#search_button').click(function(){
 		search_key = $("#search_key").val();
 		if(search_key.length == 0){
+			$("#search_content").html('')
+			$("#alert_msg").text('Search key is empty')
 			$(".alert").removeClass('fade');
 		}
 		else{ 
@@ -11,14 +13,30 @@ $(document).ready(function(){
 	         success: function(result) {
 	            if(result.api_success){
 	            	if(result.count == 0){
+	            		$("#search_content").html('')
 	            		$("#alert_msg").text("No Result Found")
 	              		$(".alert").removeClass('fade');
 	            	}
 	            	else{
 	            		$("#alert_msg").text(result.msg)
 	              		$(".alert").removeClass('fade');
+	              		html = "<h4 style='padding-top: 10px;'>" + result.count + " results found for " + search_key +"</h4>"
+	              		for(var i=0;i<result.data.length;i++)
+	              		{
+	              			song = result.data[i]
+	              			html = html + '<div class="row">' + 
+                    			   		'<div class="col-md-12" style="margin-top: 10px">' + 
+                      						'<h3>' + song.TITLE + '</h3>' +
+                      						'<h4>By ' + song.ARTIST + '</h4>' +
+                      						'<h6>' + song.TITLE + ' composed by '+ song.COMPOSER + ' from the album ' + song.ALBUM +'.</h6>' +
+                      						'<hr>' +
+                    					'</div>' +
+                  					'</div>' 
+	              		}
+	              		$("#search_content").html(html);
 	            	}
 	            }else{
+	              $("#search_content").html('')
 	              $("#alert_msg").text(result.msg)
 	              $(".alert").removeClass('fade');
 	            }
@@ -33,5 +51,58 @@ $(document).ready(function(){
 
 	$('.alert_close').click(function(){
 		$(".alert").addClass('fade');
+	});
+
+
+
+	$('.facet').click(function(){
+		$('.facet').css('text-decoration', "")
+		$('.facet').css('font-size', '1rem');
+		$(this).css('text-decoration', 'underline');
+		$(this).css('font-size', '15px');
+		search_field = $(this).attr('data-type')
+		search_key = $(this).attr('data-value')
+		
+		$.ajax({
+         url: "/search?key=" + search_key + "&field=" + search_field,
+         type: "GET",
+         success: function(result) {
+            if(result.api_success){
+            	if(result.count == 0){
+            		$("#search_content").html('')
+            		$("#alert_msg").text("No Result Found")
+              		$(".alert").removeClass('fade');
+            	}
+            	else{
+            		$("#alert_msg").text(result.msg)
+              		$(".alert").removeClass('fade');
+              		html = "<h4 style='padding-top: 10px;'>" + result.count + " results found for " + search_key +"</h4>"
+              		for(var i=0;i<result.data.length;i++)
+              		{
+              			song = result.data[i]
+              			// url = song.TITLE.split(" ").join('+')
+              			html = html + '<div class="row">' + 
+                  			   		'<div class="col-md-12" style="margin-top: 10px">' + 
+                    						'<h3>' + song.TITLE + '</h3>' +
+                    						'<h4>By ' + song.ARTIST + '</h4>' +
+                    						'<h6>' + song.TITLE + ' composed by '+ song.COMPOSER + ' from the album ' + song.ALBUM +'.</h6>' +
+                    						// '<h6>Download: <a target="_blank" href="https://s3.amazonaws.com/inf551-project/Songs-II/01+-++' + url + '.mp3">'+ song.TITLE +'</a></h6>' +  
+                    						'<hr>' +
+                  					'</div>' +
+                					'</div>' 
+              		}
+              		$("#search_content").html(html);
+            	}
+            }else{
+              $("#search_content").html('')
+              $("#alert_msg").text(result.msg)
+              $(".alert").removeClass('fade');
+            }
+         },
+         error: function(xhr, ajaxOptions, thrownError) {
+             console.log("status " + xhr.status);
+             console.log("error " + thrownError);
+         }
+        });
 	});
 });
--- inf551/inf551/views.py
+++ inf551/inf551/views.py
@@ -15,14 +15,25 @@ def search(request):
 	mongo_client = MongoClient('18.207.211.25', 27017)
 	db = mongo_client.metadata
 
-	if 'key' not in query_dict or query_dict['key'] == "" and query_dict['key'] == None :
-		return {'api_success': False, 'msg': 'Search key is empty'}
+	if 'field'in query_dict:
+		if 'key' not in query_dict or query_dict['key'] == "" and query_dict['key'] == None :
+			return {'api_success': False, 'msg': 'Search key is empty'}
+		else:
+			search_field = query_dict['field']
+			search_key = query_dict['key']
+
+			cur = db.records.find({search_field: {'$regex': ".*" + search_key + ".*", '$options': 'i'}}, {'_id':0})
+			songs = [x for x in cur]
+			cur.close()
+			return {'api_success': True, 'data': songs, 'count': len(songs), 'msg': 'Search is successful'}
 	else:
-		search_song = query_dict['key']
-		#search_song = "Talaash"
+		if 'key' not in query_dict or query_dict['key'] == "" and query_dict['key'] == None :
+			return {'api_success': False, 'msg': 'Search key is empty'}
+		else:
+			search_song = query_dict['key']
 
-		cur = db.records.find({'ALBUM' : search_song}, {'_id': 0})
-		songs = [x for x in cur]
-		cur.close()
+			cur = db.records.find({'$or': [{'ALBUM': {'$regex': ".*" + search_song + ".*", '$options': 'i'}}, {'TITLE': {'$regex': ".*" + search_song + ".*", '$options': 'i'}}, {'ARTIST': {'$regex': ".*" + search_song + ".*", '$options': 'i'}}, {'COMPOSER': {'$regex': ".*" + search_song + ".*", '$options': 'i'}}]}, {'_id':0})
+			songs = [x for x in cur]
+			cur.close()
 
-		return {'api_success': True, 'data': songs, 'count': len(songs), 'msg': 'Search is successful'}
+			return {'api_success': True, 'data': songs, 'count': len(songs), 'msg': 'Search is successful'}
